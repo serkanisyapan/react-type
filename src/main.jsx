4bdf4fe918c17a38b8185c958fa4ce70dd341fde
Serkan İşyapan <serkanisyapan1@hotmail.com>
2023-01-12 17:40:20 +0300

    fix: font loads late

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import "@fontsource/roboto-mono";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
